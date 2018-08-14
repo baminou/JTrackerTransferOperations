beca0d99107f6452c5ef76e2bf33afed2a7a804f
Hardeep Nahal <hardeep.nahal@oicr.on.ca>
2018-08-14 14:18:16 -0400

    Changed fileType to ega_entity_type, and added validation check for argument's values

--- operations/ega/to_stage/__init__.py
+++ operations/ega/to_stage/__init__.py
@@ -20,7 +20,7 @@ class ToStage(YmlConfigOperation):
 
     def _parser(self, main_parser):
         main_parser.add_argument('-a', '--audit', dest='audit', required=True)
-        main_parser.add_argument('-t', '--filetype', dest='fileType', required=True)
+        main_parser.add_argument('-t', '--ega_entity_type', dest='ega_entity_type', required=True, help="This argument only accept one of two values: run or analysis", choices=['run','analysis'])
         main_parser.add_argument('-o', '--output-file', dest='output_file', required=True)
 
     def _config_schema(self):
@@ -83,7 +83,7 @@ class ToStage(YmlConfigOperation):
         # Load all EGAFIDs from the audit csv file
         audit_fids = ega_transfer.get_audit_fids(self.args.audit)
 
-        to_stage = ega_transfer.get_files_to_stage(list(set(audit_fids) - set(ega_box_fids + jtracker_fids)), self.args.audit, self.args.fileType)
+        to_stage = ega_transfer.get_files_to_stage(list(set(audit_fids) - set(ega_box_fids + jtracker_fids)), self.args.audit, self.args.ega_entity_type)
         with open(self.args.output_file, 'w') as fp:
             dict_writer = csv.DictWriter(fp, to_stage[0].keys(), delimiter='\t')
             dict_writer.writeheader()
