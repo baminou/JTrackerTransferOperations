bcde2ec905538d08f324ccfc51cf33a3aba5bd97
baminou <brice.aminou@gmail.com>
2018-08-27 11:40:45 -0400

    Fix

--- operations/base/__init__.py
+++ operations/base/__init__.py
@@ -8,7 +8,6 @@ from .list_operation import Listoperation
 from .yaml_to_json import Yamltojson
 from .publish import Publish
 from .install_operation import Installoperation
-from .install_library import Installlibrary
 from .install import Install
 
 class Base(Library):
@@ -31,6 +30,5 @@ class Base(Library):
             'yaml_to_json': Yamltojson,
             'publish': Publish,
             'install:operation': Installoperation,
-            'install:library': Installlibrary,
             'install': Install
         }
