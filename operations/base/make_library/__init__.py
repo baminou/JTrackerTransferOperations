da2e23780938e19aabb325d41b73973712f8ad16
baminou <brice.aminou@gmail.com>
2018-08-27 11:23:15 -0400

    Make library resource template added

--- operations/base/make_library/__init__.py
+++ operations/base/make_library/__init__.py
@@ -1,6 +1,8 @@
 
 from operation_types.operation import Operation
 from ..utils import library_create
+import shutil
+import os
 
 class Makelibrary(Operation):
 
@@ -20,5 +22,15 @@ class Makelibrary(Operation):
         return
 
     def _run(self):
-        library_create(self.args.name)
+        library_name = self.args.name
+        library_template_path = os.path.join(os.path.dirname(os.path.relpath(__file__)),'resources','template')
+        library_new_path = os.path.join('operations',str(library_name).lower())
+        library_new_init = os.path.join(library_new_path,'__init__.py')
+        shutil.copytree(library_template_path,library_new_path)
+
+        with open(library_new_init,'r') as f:
+            new_text = f.read().replace('LIBRARYNAME', str(library_name).lower())
+
+        with open(library_new_init,'w') as f:
+            f.write(new_text)
         return
