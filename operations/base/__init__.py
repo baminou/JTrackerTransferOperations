fe8855b8824a49bc35252b57b7fecc0dcf8fbd1a
baminou <brice.aminou@gmail.com>
2018-09-06 10:10:48 -0400

    Installation operation added to install all pip requirements

--- operations/base/__init__.py
+++ operations/base/__init__.py
@@ -7,7 +7,6 @@ from .make_operationtype import MakeOperationType
 from .list_operation import Listoperation
 from .yaml_to_json import Yamltojson
 from .publish import Publish
-from .install_operation import Installoperation
 from .install import Install
 from .command import Command
 
@@ -30,7 +29,6 @@ class Base(Library):
             'list:operations': Listoperation,
             'yaml_to_json': Yamltojson,
             'publish': Publish,
-            'install:operation': Installoperation,
             'install': Install,
             'command': Command
         }
--- operations/base/install/__init__.py
+++ operations/base/install/__init__.py
@@ -1,6 +1,7 @@
 
 from operation_types.operation import Operation
 from operations.library import Library
+import subprocess
 
 class Install(Operation):
 
@@ -13,11 +14,19 @@ class Install(Operation):
         return "Install has not been documented yet."
 
     def _parser(self, main_parser):
+        main_parser.add_argument('pip_version',help="Command of pip to run for packages eg. pip, pip3, ...")
         return
 
     def _run(self):
-        for library_class in Library.__subclasses__():
-            for key in library_class.operations():
-                print(library_class.name() + " - "+key)
-                library_class.operations()[key].install()
+        try:
+            subprocess.check_output(['pipreqs', '.'])
+        except OSError as e:
+            raise Exception("Install pipreqs, run: pip install pipreqs")
+
+        try:
+            subprocess.check_output([self.args.pip_version, 'install', '-r', 'requirements.txt'])
+        except OSError as e:
+            raise Exception("Program missing, install %s" % (self.args.pip_version))
+
+
         return True
